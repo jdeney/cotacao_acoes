6877daf1146de6554b75929016c0f3b668a5ec38
Deney Araujo <deneyaraujo@gmail.com>
2024-09-20 16:21:52 -0300

    Update extrator_cotacoes_acoes.py

--- extrator_cotacoes_acoes.py
+++ extrator_cotacoes_acoes.py
@@ -1,6 +1,3 @@
-#Obs: não somos responsáveis pela violação de qualquer direito autoral com o uso desse programa, use por sua
-#responsabilidade, e lembre-se de dar crédito ao www.fundamentus.com.br
-
 import argparse
 from tqdm import tqdm
 from selenium.webdriver.chrome.service import Service
@@ -10,40 +7,54 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import pandas as pd
 from datetime import datetime
+from io import StringIO  # Adicionar a importação para StringIO
 
 # Versão do programa
-VERSION = ['1.0.0']
+VERSION = ['1.0.1']
 
 # Options
-parser = argparse.ArgumentParser(description='Extrator de dados de Cotaçoes de tickers a partir do fundamentus.com.br.')
+parser = argparse.ArgumentParser(description='Extrator de dados de Cotações de tickers a partir do fundamentus.com.br.')
 parser.add_argument('-v', '--version', action='version', version=f'%(prog)s {VERSION}')
-parser.add_argument('-o', '--output', help='Especifique o nome de saíde do seu arquivo. O Default é fundamentus_tickers.csv.', default='fundsexplorer_ranking.csv')
+parser.add_argument('-o', '--output', help='Especifique o nome de saída do seu arquivo. O Default é fundamentus_tickers.csv.', default='fundsexplorer_ranking.csv')
 args = parser.parse_args()
 
 print(f'Started in:   {datetime.now().strftime("%B %d, %Y %H:%M:%S")}\n')
 start_time = datetime.now()
 
+# Função para configurar e instalar o driver
 def setUpdate():
     try:
-        Service(ChromeDriverManager().install()) #instala o webdrive com a mesma versao
+        driver_path = ChromeDriverManager().install()  # Instala o WebDriver atualizado
+        return driver_path
     except Exception as e:
         print(f'Error Install: {e}')
+        return None
 
-# status_bar
+# Status bar
 progress_bar = tqdm(total=6, bar_format='{l_bar}{bar}| {percentage:3.0f}% ')
 
 # Inicialize o driver do Selenium
-setUpdate()
+driver_path = setUpdate()
+
+if not driver_path:
+    print("Erro ao instalar o WebDriver. Verifique sua conexão ou permissões.")
+    exit(1)
 
 progress_bar.update(1)  # Update
 
-serv = Service(ChromeDriverManager().install()) #instala o webdrive com a mesma versao
+# Configurações de opções do Chrome
 options = Options()
 options.add_argument("--lang=en")
 options.add_argument('--headless')
 options.add_argument("--incognito")
+
+# Inicialize o ChromeDriver com as opções e serviço configurados
+serv = Service(driver_path)
 driver = webdriver.Chrome(service=serv, options=options)
 
+progress_bar.update(1)  # Update
+
+# URL alvo
 url = "https://www.fundamentus.com.br/resultado.php"
 
 # Navegue até a página
@@ -51,7 +62,7 @@ driver.get(url)
 
 progress_bar.update(1)  # Update
 
-# Espere até que a tabela esteja carregada (pode ser necessário aumentar esse tempo)
+# Espere até que a tabela esteja carregada (pode ser necessário ajustar o tempo)
 driver.implicitly_wait(10)
 
 # Extraia a tabela
@@ -59,15 +70,16 @@ table = driver.find_element(By.CSS_SELECTOR, '.resultado.rowstyle-par.colstyle-c
 
 progress_bar.update(1)  # Update
 
-# Crie um DataFrame do Pandas a partir dos dados da tabela
-tickers = pd.read_html(table.get_attribute('outerHTML'), encoding='utf-8', decimal=".", thousands='.')[0]
+# Usar StringIO para evitar o FutureWarning
+table_html = table.get_attribute('outerHTML')
+tickers = pd.read_html(StringIO(table_html), encoding='utf-8', decimal=".", thousands='.')[0]
 
 # Feche o driver
 driver.quit()
 
 progress_bar.update(1)  # Update
 
-tickers = tickers.dropna(axis=1, how='all') #remove colunas vazias
+tickers = tickers.dropna(axis=1, how='all')  # Remove colunas vazias
 
 progress_bar.update(1)  # Update
 
@@ -80,5 +92,3 @@ progress_bar.close()  # close
 
 print(f'\nTotal time:    {str(datetime.now() - start_time).split(".")[0]}')
 print(f'Finished at:   {datetime.now().strftime("%B %d, %Y %H:%M:%S")}\n')
-
-
